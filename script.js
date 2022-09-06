efde52a2601340ee501356bb157381df4190c4a0
Paula Rodrigues <paula.uninter@gmail.com>
2022-09-06 16:15:49 -0300

    implement JS + css for form validation

---
+++ script.js
@@ -0,0 +1,61 @@
+function isValidEmail(email) {
+  return /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email);
+}
+
+function processInput(input) {
+  if (isValidInput(input)) {
+    if (input.div.classList.contains("fail")) {
+      input.div.classList.remove("fail");
+    }
+    input.div.classList.add("success");
+    return true;
+  }
+
+  if (input.div.classList.contains("success")) {
+    input.div.classList.remove("success");
+  }
+  input.div.classList.add("fail");
+  return false;
+}
+
+function isValidInput(input) {
+  if (input.input.value === "") {
+    input.err.textContent = `${input.name} cannot be empty`;
+    return false;
+  }
+
+  if (input.name === "Email" && !isValidEmail(input.input.value)) {
+    input.err.textContent = "Looks like this is not an email";
+    return false;
+  }
+
+  input.err.textContent = "";
+  return true;
+}
+
+function handleForm() {
+  const inputs = [firstName, lastName, email, password];
+  const validation = inputs.map(processInput);
+  if (!validation.includes(false)) {
+    alert("Success!");
+  }
+}
+
+const firstName = {div: document.querySelector(".first-name"), name: "First Name", isEmail: false};
+firstName.input = firstName.div.querySelector("input");
+firstName.err = firstName.div.querySelector(".err");
+
+const lastName = {div: document.querySelector(".last-name"), name: "Last Name", isEmail: false};
+lastName.input = lastName.div.querySelector("input");
+lastName.err = lastName.div.querySelector(".err");
+
+const email = {div: document.querySelector(".email"), name: "Email", isEmail: true};
+email.input = email.div.querySelector("input");
+email.err = email.div.querySelector(".err");
+
+const password = {div: document.querySelector(".password"), name: "Password", isEmail: false};
+password.input = password.div.querySelector("input");
+password.err = password.div.querySelector(".err");
+
+const button = document.querySelector("button");
+button.addEventListener("click", handleForm);
